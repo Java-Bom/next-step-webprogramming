7239479b93174254d09399d380efbcfa17336d48
chanin <pci2676@gmail.com>
2020-06-13 16:42:23 +0900

    refactor : IndexResponseHandler 변경

--- 3~6장/web-application-server/src/main/java/webserver/handler/IndexResponseHandler.java
+++ 3~6장/web-application-server/src/main/java/webserver/handler/IndexResponseHandler.java
@@ -1,15 +1,9 @@
 package webserver.handler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
 
 public class IndexResponseHandler implements ResponseHandler {
-    private static final Logger log = LoggerFactory.getLogger(IndexResponseHandler.class);
     private final String htmlName;
 
     public IndexResponseHandler(String htmlName) {
@@ -17,21 +11,8 @@ public class IndexResponseHandler implements ResponseHandler {
     }
 
     @Override
-    public void response(final DataOutputStream dos, final String bodyString) throws IOException {
-        byte[] body = Files.readAllBytes(new File("./webapp/" + htmlName).toPath());
-        response200Header(dos, body.length);
-        dos.write(body, 0, body.length);
-        dos.flush();
+    public void response(final HttpRequest httpRequest, final HttpResponse httpResponse) {
+        httpResponse.forward(htmlName);
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
 }
