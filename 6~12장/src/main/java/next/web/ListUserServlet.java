bc53dcdfa3547bef6a3a2e5a24281ada30006e59
Gobukgol <zxczc159@gmail.com>
2020-07-11 16:41:40 +0900

    step0 생성

--- 6~12장/src/main/java/next/web/ListUserServlet.java
+++ 6~12장/src/main/java/next/web/ListUserServlet.java
@@ -1,19 +1,24 @@
-package next;
+package next.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+import core.db.DataBase;
+
+@WebServlet("/user/list")
+public class ListUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("Hello World!");
+        req.setAttribute("users", DataBase.findAll());
+        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+        rd.forward(req, resp);
     }
 }
